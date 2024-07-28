a36fe69e1a4fb9332016d41778d7c7a54d8370dc
123Deepanshi <125633956+123Deepanshi@users.noreply.github.com>
2024-07-28 11:40:08 +0530

    Add files via upload

---
+++ Ecomm_Book_1031.DataAccess/Data/Migrations/20231212080846_AddSPForCoverType.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Runtime.InteropServices;
+
+#nullable disable
+
+namespace Ecomm_Book_1031.DataAccess.Migrations
+{
+    public partial class AddSPForCoverType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROCEDURE SP_Create_CoverType
+                                 @name varchar(50)
+	
+                                 AS
+	                             insert CoverTypes values(@name)");
+            migrationBuilder.Sql(@"CREATE PROCEDURE SP_Update_CoverType
+                                 @id int,
+                                 @name varchar(50)
+	
+                                 AS
+	                             Update CoverTypes set name=@name where id=@id");
+            migrationBuilder.Sql(@"CREATE PROCEDURE SP_Delete_CoverType
+                                 @id int
+	
+                                 AS
+	                             Delete CoverTypes where id=@id");
+            migrationBuilder.Sql(@"CREATE PROCEDURE SP_Get_CoverTypes
+                                 AS
+	                             select * from CoverTypes");
+            migrationBuilder.Sql(@"CREATE PROCEDURE SP_Get_CoverType
+                                 @id int
+	
+                                 AS
+	                             select * from CoverTypes where id=@id");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Ecomm_Book_1031.Utility/SD.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecomm_Book_1031.Utility
+{
+    public static class SD
+    {
+        //Cover Type store procedure
+        public const string Proc_GetCoverTypes = "SP_Get_CoverTypes";
+        public const string Proc_GetCoverType = "SP_Get_CoverType";
+        public const string Proc_CreateCoverType= "SP_Create_CoverType";
+        public const string Proc_UpdateCoverType = "SP_Update_CoverType";
+        public const string Proc_DeleteCoverType = "SP_Delete_CoverType";
+
+        //Roles
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee User";
+        public const string Role_Company = "Company User";
+        public const string Role_Individual = "Individual User";
+
+        //Order Status
+        public const string OrderStatusPending = "Pending";
+        public const string OrderStatusApproved = "Approved";
+        public const string OrderStatusInProgress = "Processing";
+        public const string OrderStatusShipped = "Shipped";
+        public const string OrderStatusCancelled = "Cancelled";
+        public const string OrderStatusRefunded = "Refunded";
+
+        //Payment Status
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayPayment = "PaymentStatusDelay";
+        public const string PaymentStatusRejected = "Rejected";
+
+        //Session
+        public const string Ss_CartSessionCount = "CartCountSession";
+
+        public static double GetPriceBasedQuantity(double quantity,double price,double price50,double price100)
+        {
+            if (quantity < 50)
+                return price;
+            else if (quantity <100)
+                return price50;
+            else return price100;
+        }
+    }
+}
